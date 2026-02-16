761d22687998a034ce2872ac9d93ef1bc85f96a1
emanuel937 <emanuelabizimi@gmail.com>
2026-02-16 08:54:56 +0000

    adding new features

--- includes/helpers/checker.php
+++ includes/helpers/checker.php
@@ -1,32 +1,46 @@
 <?php 
 
-class Checker{
+class XSAUpdateChecker{
 
     static function updateVersion($transient){
+        
+        delete_site_transient('update_plugins');
+        if (empty($transient->checked)) {
+            return $transient;
+        }
 
-       if (empty($transient->checked)) return $transient;
+        // Version actuelle du plugin
+        $current_version = $transient->checked[__PLUGINS_FILE__];
 
-       $plugins_file     =  $transient->checked[__PLUGINS_FILE__];
-       $remote_json      =  wp_remote_get("https://x-studioapp.fr/plugins/xstudioapp-widget/update.json");
-       $json_body        = $remote_json;
-  
-    }
+        // Récupération du JSON distant
+        $response = wp_remote_get("https://x-studioapp.fr/plugins/xstudioapp-widget/update.json");
 
-    static function license(){
+        if (is_wp_error($response)) {
+            return $transient;
+        }
 
-    }
+        $json = json_decode(wp_remote_retrieve_body($response));
 
+        if (!$json) {
+            return $transient;
+        }
 
-    static function upgrade(){
+        // Comparer les versions
+        if (version_compare($current_version, $json->version, '<')) {
+
+            $plugin_info = new stdClass();
+            $plugin_info->slug = 'xstudioapp-widget';
+            $plugin_info->plugin = __PLUGINS_FILE__;
+            $plugin_info->new_version = $json->version;
+            $plugin_info->url = "https://x-studioapp.fr/plugins/xstudioapp-widget/";
+            $plugin_info->package = $json->download_url;
+
+            // Injecter dans WordPress
+            $transient->response[__PLUGINS_FILE__] = $plugin_info;
+        }
+
+        return $transient;
     }
 }
 
-
-
-Checker::license();
-
-
-
-
-
-add_filter('pre_set_site_transient_update_plugins', ['Checker', 'updateVersion']);
+add_filter('pre_set_site_transient_update_plugins', ['XSAUpdateChecker', 'updateVersion']);
